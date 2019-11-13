fc6954cb38f7cdd3b37d5ce79deff11e1241aa1d
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-11-13 13:16:29 -0300

    First commit in PR

--- farmacia.cpp
+++ farmacia.cpp
@@ -7,10 +7,37 @@ void menu();
 void le_dados();
 void cadastro(int codigo, char *remedio, float preco, int quantidade, char generico, char *categoria, char *fabricante);
 void exibir_dados();
+void consulta_dados();
 
 int main(){
     menu();
     return 0;
+}
+
+void menu(){
+    int opcao = 1;  
+    while (opcao != 0){
+        limpa_tela();
+        printf("1 - Cadastrar\n");
+        printf("2 - Consultar\n");
+        printf("3 - Alterar\n");
+        printf("4 - Remover\n");
+        printf("5 - Exibir\n");
+        printf("0 - Exit\n");
+        printf("Escolha uma das opções: ");
+        scanf("%d", &opcao);
+        switch (opcao){
+            case 1:
+                le_dados();
+                break;
+            case 2:
+                consulta_dados();
+                break;
+            case 5:
+                exibir_dados();
+                break;
+        }
+    }
 }
 
 void le_dados(){
@@ -56,29 +83,6 @@ void le_dados(){
     }
 }
 
-void menu(){
-    int opcao = 1;  
-    while (opcao != 0){
-        limpa_tela();
-        printf("1 - Cadastrar\n");
-        printf("2 - Consultar\n");
-        printf("3 - Alterar\n");
-        printf("4 - Remover\n");
-        printf("5 - Exibir\n");
-        printf("0 - Exit\n");
-        printf("Escolha uma das opções: ");
-        scanf("%d", &opcao);
-        switch (opcao){
-            case 1:
-                le_dados();
-                break;
-            case 5:
-                exibir_dados();
-                break;
-        }
-    }
-}
-
 void cadastro(int codigo, char *remedio, float preco, int quantidade, char generico, char *categoria, char *fabricante){
     FILE *file = fopen("txt/data_base_pharma.txt", "a");
     if (!file){
@@ -104,6 +108,10 @@ void exibir_dados(){
     int take_break = 0;
     printf("\nPress any key to back to menu: ");
     scanf("%d", &take_break);
+}
+
+void consulta_dados(){
+    printf("Need build this functions.\n");
 }
 
 void limpa_tela(){
